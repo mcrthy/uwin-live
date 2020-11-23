cdfcf06081b1a103bdc6578db9a35124edc18419
Matt McCarthy <contact@mattmccarthy.net>
2020-11-23 14:57:31 -0500

    pushing code for TA review

--- chat.py
+++ chat.py
@@ -115,7 +115,6 @@ def main():
             (r"/a/message/new", MessageNewHandler),
             (r"/a/message/updates", MessageUpdatesHandler),
         ],
-        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         xsrf_cookies=True,
